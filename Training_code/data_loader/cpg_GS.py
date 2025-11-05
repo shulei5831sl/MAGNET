91fab0e62d9e88232c31ed6179df535f5fd1cd1b
shulei <shulei58317@gmail.com>
2025-11-06 00:48:07 +0800

    Adjust Devign generator for MAGNET requirements

--- Training_code/data_loader/cpg_GS.py
+++ Training_code/data_loader/cpg_GS.py
@@ -463,115 +463,100 @@ def var_combine(ast_edges, same_var):
     return ast_combine_edges
 
 def graphGeneration(nodes, edges, edge_type_map, ver_edge_type_map):
-    index_map = dict()
-    index_map_ver = dict()
-    all_nodes = set()
-    all_ast_edges = []
-    all_edges = list()
+    index_lookup = {}
+    index_map_ver = {}
+    ast_blocks = []
+    all_edges = []
     for node in nodes:
         if node['isCFGNode'].strip() != 'True' or node['key'].strip() == 'File':
             continue
-        all_nodes.add(node['key'])  
-        # entry  exit
-        if node['type'] in ['CFGEntryNode','CFGExitNode']:
+        if node['type'] in ['CFGEntryNode', 'CFGExitNode']:
             continue
-        nodeKey = [node['key']]
+        node_key = node['key'].strip()
         ast_edges = []
-        build_ast(nodeKey, edges, ast_edges)
-  
+        build_ast([node_key], edges, ast_edges)
         if len(ast_edges) == 0:
-            #break; continue; returns; (;;) 
             if spe_sent(node['type'], node['code'].strip().split()):
-                dic = {}
-                dic['start'] = nodeKey[0]
-                dic['end'] = nodeKey
-                ast_edges.append(dic)
+                ast_edges.append({'start': node_key, 'end': [node_key]})
             else:
                 return None, None, None, True
-        all_ast_edges.append(ast_edges)  
-    edge_count_before = 0
-    node_count_before = set()
-    for item in all_ast_edges:
-        for ast in item:
-            node_count_before.add(ast['start'])
-            node_count_before = node_count_before | set(ast['end'])
-            edge_count_before += len(ast['end'])
-    step1_ast_edges = []
-    for i in all_ast_edges:    
-        # break; continue; return
-        if len(i) == 1 and i[0]['start'] == i[0]['end'][0] and len(i[0]['end']) == 1:
-            step1_ast_edges.append(i)
-        else:
-            new_edges = ast_prune(nodes, i, 'IS_AST_PARENT')
-            step1_ast_edges.append(new_edges)
-    same_var = get_same_var(step1_ast_edges, nodes)
-    step2_ast_edges = var_combine(step1_ast_edges, same_var)
+        ast_blocks.append(ast_edges)
 
-    edge_count_after = 0
-    node_count_after = set()
-    for item in step2_ast_edges:
-        for ast in item:
-            node_count_after.add(ast['start'])
-            node_count_after = node_count_after | set(ast['end'])
-            edge_count_after += len(ast['end'])
-    #add nsc
-    nsc_edges_ = get_ncs_edges(step1_ast_edges, 'NSC', nodes)
-    nsc_edges = get_combine_ncs_edges(step1_ast_edges, 'NSC', nodes, same_var)
     ast_type = 'IS_AST_PARENT'
+    for block in ast_blocks:
+        for relation in block:
+            start = relation['start']
+            for end in relation['end']:
+                all_edges.append([start, ast_type, end])
 
-    for item in step2_ast_edges:
-        # break; continue; return; 
-        if len(item) == 1 and len(item[0]['end']) == 1 and item[0]['start'] == item[0]['end'][0]:
-            continue
-        for x in item:
-            start = x['start']
-            for end in x['end']:
-                all_edges.append([start, ast_type, end])  
+    if 'NSC' in edge_type_map:
+        nsc_edges = get_ncs_edges(ast_blocks, 'NSC', nodes)
+    else:
+        nsc_edges = []
 
-    for e in edges:
-        start, end, eType = e['start'], e['end'], e['type']    
+    for edge in edges:
+        start = edge['start'].strip()
+        end = edge['end'].strip()
+        e_type = edge['type'].strip()
         start_node = get_nodes_by_key(nodes, start)
         end_node = get_nodes_by_key(nodes, end)
+        if start_node is None or end_node is None:
+            continue
         if start_node['isCFGNode'].strip() != 'True' or end_node['isCFGNode'].strip() != 'True':
             continue
-        if eType != 'IS_FILE_OF' and eType != ast_type:
-            if not eType in edge_type_map: #or not start in all_nodes or not end in all_nodes:
+        if e_type in ['IS_FILE_OF', ast_type]:
+            continue
+        if e_type not in edge_type_map:
+            continue
+        all_edges.append([start, e_type, end])
+
+    all_nodes = set()
+    for s, _, t in all_edges:
+        all_nodes.add(s)
+        all_nodes.add(t)
+    for s, _, t in nsc_edges:
+        all_nodes.add(s)
+        all_nodes.add(t)
+
+    if len(all_nodes) == 0:
+        return None, None, None, None
+
+    try:
+        sorted_nodes = sorted(all_nodes, key=lambda x: int(x))
+    except ValueError:
+        sorted_nodes = sorted(all_nodes)
+
+    for idx, node_key in enumerate(sorted_nodes):
+        index_lookup[node_key] = idx
+        index_map_ver[idx] = node_key
+
+    converted_edges = []
+    for s, e_type, t in all_edges:
+        if s not in index_lookup or t not in index_lookup:
+            continue
+        converted_edges.append([index_lookup[s], edge_type_map[e_type], index_lookup[t]])
+
+    if 'NSC' in edge_type_map:
+        for s, e_type, t in nsc_edges:
+            if s not in index_lookup or t not in index_lookup:
                 continue
-            all_edges.append([start, eType, end])  
-    for e in all_edges:
-        start, _, end = e
-        all_nodes.add(start)
-        all_nodes.add(end)
-    if len(all_nodes) == 0 or len(all_nodes) > 500:  # V1是750
+            converted_edges.append([index_lookup[s], edge_type_map[e_type], index_lookup[t]])
+
+    if 'Self_loop' in edge_type_map:
+        for node_key in sorted_nodes:
+            converted_edges.append([index_lookup[node_key], edge_type_map['Self_loop'], index_lookup[node_key]])
+
+    if len(converted_edges) == 0:
         return None, None, None, None
-    for i, node in enumerate(all_nodes):
-        index_map[node] = i
-        index_map_ver[i] = node
-    all_edges_new = []   #original full graph
-    for e in all_edges: # e = [start, type, end]
-        e_new = [index_map[e[0]], edge_type_map[e[1]], index_map[e[2]]]
-        all_edges_new.append(e_new)
-    #add nsc_edge
-    for e in nsc_edges:
-        e_new = [index_map[e[0]], edge_type_map[e[1]], index_map[e[2]]]
-        all_edges_new.append(e_new)
-    #add self-loop
-    loop = 'Self_loop'
-    for node in all_nodes:
-        self_loop = [index_map[node], edge_type_map[loop], index_map[node]]
-        all_edges_new.append(self_loop)
-    if len(all_edges_new) == 0:
-        return None, None, None, None
-    edges_num = {}
-    for t in edge_type_map.keys():
-        edges_num[t] = 0
-    for e in all_edges_new:
-        key = ver_edge_type_map[str(e[1])]
-        edges_num[key] += 1
-    edges_num['ast_reduced'] = edge_count_before - edge_count_after
-    edges_num['nsc_reduced'] = len(nsc_edges_) - len(nsc_edges)
-    edges_num['nodes_reduced'] = len(node_count_before) - len(node_count_after)
-    return index_map_ver, all_edges_new, len(index_map_ver), edges_num
+
+    edges_num = {etype: 0 for etype in edge_type_map.keys()}
+    for _, edge_type_id, _ in converted_edges:
+        type_name = ver_edge_type_map.get(str(edge_type_id))
+        if type_name is not None and type_name in edges_num:
+            edges_num[type_name] += 1
+    edges_num['nodes'] = len(sorted_nodes)
+
+    return index_map_ver, converted_edges, len(index_map_ver), edges_num
 
 def word2vec(nodes, index_map, graph, wv):
     gInput = list()
@@ -603,8 +588,10 @@ def word2vec(nodes, index_map, graph, wv):
             fnrp = np.divide(nrp, len(tokens))
         else:
             fnrp = nrp
-        #fnrp = np.concatenate((fnrp, one_hot), axis = 0)
-        gInput.append(fnrp.tolist())
+        node_type = node.get('type', '').strip()
+        type_id = node_type_map.get(node_type, 0)
+        feature_vector = np.concatenate((fnrp, np.array([float(type_id)])), axis=0)
+        gInput.append(feature_vector.tolist())
     return gInput
 
 def visual_graph(all_nodes, index_map, edges, allowed_edge_types, ver_edge_type,  file_name = 'graph'):
@@ -703,7 +690,20 @@ def main():
             file_name = entry['file_path']
             nodes_path = os.path.join(args.csv, file_name, 'nodes.csv')
             edges_path = os.path.join(args.csv, file_name, 'edges.csv')
-            label = int(entry['label'])
+            label_value = entry.get('label')
+            if label_value is None:
+                label_value = entry.get('target')
+            if label_value is None and 'targets' in entry:
+                targets_field = entry['targets']
+                if isinstance(targets_field, list) and len(targets_field) > 0:
+                    first = targets_field[0]
+                    if isinstance(first, list) and len(first) > 0:
+                        label_value = first[0]
+                    else:
+                        label_value = first
+            if label_value is None:
+                continue
+            label = int(label_value)
             if not os.path.exists(nodes_path) or not os.path.exists(edges_path):
                 continue
             nodes = read_csv(nodes_path)
